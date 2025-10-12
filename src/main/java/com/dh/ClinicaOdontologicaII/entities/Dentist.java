2d2ac69e5f7adefd9d1a82e19fd439b434a07d6b
MP Data - Trading <mpiccato@me.com>
2025-10-12 17:43:10 -0300

    configuradas la primeras entidades

---
+++ src/main/java/com/dh/ClinicaOdontologicaII/entities/Dentist.java
@@ -0,0 +1,63 @@
+package com.dh.ClinicaOdontologicaII.entities;
+
+
+import jakarta.persistence.*;
+
+//Anotaciones
+@Entity
+@Table(name = "Dentists")
+public class Dentist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dentist_id")
+    private Integer id;
+
+    @Column(name = "registration")
+    private Integer registration;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "latName")
+    private String lastName;
+
+    //Constructor por defecto vacío
+    public Dentist() {
+    }
+
+    // Getters y Setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(Integer registration) {
+        this.registration = registration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
