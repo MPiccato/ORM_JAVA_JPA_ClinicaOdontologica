5240300c6cd8af073e9d20d58db79df248b8c7e3
MP Data - Trading <mpiccato@me.com>
2025-10-15 09:04:55 -0300

    configurando el ORM

---
+++ src/main/java/com/dh/ClinicaOdontologicaII/services/PatientService.java
@@ -0,0 +1,35 @@
+package com.dh.ClinicaOdontologicaII.services;
+
+import com.dh.ClinicaOdontologicaII.entities.Patient;
+import com.dh.ClinicaOdontologicaII.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PatientService {
+    private PatientRepository repository;
+
+    @Autowired
+    public PatientService(PatientRepository repository) {
+        this.repository = repository;
+    }
+
+    public Patient save(Patient patient) {
+        return repository.save(patient);
+    }
+    public Optional<Patient> findById(Integer id) {
+        return repository.findById(id);
+    }
+    public void update(Patient patient) {
+        repository.save(patient);
+    }
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+
+    public void findAll(){
+        repository.findAll();
+    }
+}
