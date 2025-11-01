788033ecaa454aacb756cc74ced040b14e13f9aa
MP Data - Trading <mpiccato@me.com>
2025-11-01 07:59:46 -0300

    crud con JPA/Hibernate terminado

--- src/main/java/com/dh/ClinicaOdontologicaII/controller/PatientController.java
+++ src/main/java/com/dh/ClinicaOdontologicaII/controller/PatientController.java
@@ -45,4 +45,28 @@ public class PatientController {
 
     }
 
+    @GetMapping("/id")
+    public Patient findById(@RequestParam Integer id) {
+        Optional<Patient> optionalPatient = patientService.findById(id);
+        if (optionalPatient.isPresent()) {
+            return optionalPatient.get();
+
+        } else {
+            return null;
+        }
+    }
+
+    //Eliminar paciente por id
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        Optional<Patient> deletePatient = patientService.findById(id);
+        if (deletePatient.isPresent()) {
+            patientService.delete(id);
+            return "Se eliminó al paciente de la base de datos con id: " + id;
+        } else {
+            return "No se encontró paciente a eliminar con id: " + id;
+        }
+
+    }
+
 }
